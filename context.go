a946fb0ccc2790f304541b2f75d767400fcafa57
Morten Krogh <morten.krogh@amberbio.com>
2016-01-22 11:19:46 +0100

    context public methods

--- context.go
+++ context.go
@@ -1,6 +1,7 @@
 package gowsev
 
 import (
+	"time"
 	"golang.org/x/net/websocket"
 )
 
@@ -30,10 +31,26 @@ type EvContext struct {
 	conns             map[uint64]evConn
 	readerMessageChan chan evMessage
 	readerCloseChan   chan uint64
-	timeout           int // milliseconds
+	timeout           time.Duration // int64 nanoseconds
 }
 
 func makeContext(handler *Handler) EvContext {
 	return EvContext{handler, 0, make(map[uint64]evConn), make(chan evMessage), make(chan uint64), 1000}
+}
+
+func (context *EvContext) GetTimeout() time.Duration {
+	return context.timeout
+}
+
+func (context *EvContext) SetTimeout(timeout time.Duration) {
+	context.timeout = timeout
+}
+
+func (context *EvContext) AddConn(conn *websocket.Conn) {
+	go writer(conn)
+}
+
+func (context *EvContext) ListenAndServe(port string) {
+
 }
 
